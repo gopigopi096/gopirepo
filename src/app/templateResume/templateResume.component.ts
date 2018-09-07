45fb87eb5188f6a2a10335e961a6725c5e108197
gopigopi096 <gopigopi096@gmail.com>
2018-09-07 13:54:53 +0530

    Update templateResume.component.ts

--- src/app/templateResume/templateResume.component.ts
+++ src/app/templateResume/templateResume.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router } from '@angular/router';
 import {TemplateService} from '../templateResume/template.service';
+vvvvvv
 @Component({
   selector: 'app-templateResume',
   templateUrl: './templateResume.component.html',
